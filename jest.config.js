353c8cbf240cef9bca04cf642f0bc96f07223b50
Janos Mucsi <janos.mucsi@cnb.com>
2021-12-10 11:35:01 -0800

    first steps to enable tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: '@vue/cli-plugin-unit-jest',
+    testMatch: [
+        '**/__tests__/**/*.[jt]s?(x)',
+        '**/?(*.)+(spec|test).[jt]s?(x)'
+    ],
+    transform: {
+        '^.+\\.vue$': 'vue-jest'
+    }
+};
---
+++ src/HeaderBarLink.test.js
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils';
+
+
+describe('HeaderBarLink', () => {
+    it('renders correctly', () => {
+        console.log("ok", shallowMount)
+        // const wrapper = shallowMount(HeaderBarLink, {
+        //     propsData: {
+        //         href: '#foo',
+        //         textColor: '#fff'
+        //     }
+        // });
+        // expect(wrapper.html()).toMatchInlineSnapshot(
+        //     '<a href="#foo" style="color: rgb(255, 255, 255);" class="header-bar-link"></a>'
+        // );
+    });
+});
