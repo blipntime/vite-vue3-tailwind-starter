b7fb63473d8463859ae12684694d8d6c683bab67
Janos Mucsi <expert__@mailfence.com>
2021-12-10 15:38:02 -0800

    Getting versions right.

--- src/HeaderBarLink.test.js
+++ src/HeaderBarLink.test.js
@@ -1,15 +1,11 @@
 import { shallowMount } from '@vue/test-utils';
-
+import Home from './views/Home.vue';
 
 describe('HeaderBarLink', () => {
     it('renders correctly', () => {
-        console.log("ok", shallowMount)
-        // const wrapper = shallowMount(HeaderBarLink, {
-        //     propsData: {
-        //         href: '#foo',
-        //         textColor: '#fff'
-        //     }
-        // });
+
+        const wrapper = shallowMount(Home, {});
+        console.log("ok", shallowMount, Home, wrapper)
         // expect(wrapper.html()).toMatchInlineSnapshot(
         //     '<a href="#foo" style="color: rgb(255, 255, 255);" class="header-bar-link"></a>'
         // );
